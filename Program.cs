86d0c0ba1475df31ce9cf1194ad9756957519352
Tec <Tec@SKAB1-PC7>
2023-01-16 13:09:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace C_til_f
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("[C] Celsius til Fahrenheit   [F] Fahrenheit til Celsius");
+            string tast = Convert.ToString(Console.ReadKey().KeyChar).ToLower();
+            if (tast == "c")
+            {
+                Console.Clear();
+                Console.Write("Indtast Celsius: ");
+                int Cel = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                double Celresultat = Cel * 1.8 + 32;
+                Console.WriteLine($"Dit resultat er {Celresultat}, tryk and tast for at starte om");
+                Console.ReadKey();
+                Console.Clear();
+                Main(args);
+            }
+            else if (tast == "f")
+            {
+                Console.Clear();
+                Console.Write("Indtast Fehrenheit: ");
+                int Feh = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                    
+                double Fehresultat = (Feh - 32) * .5556;
+                string Bo = Fehresultat.ToString("N0");
+                Console.WriteLine($"Dit resultat er {Bo}, tryk and tast for at starte om");
+                Console.ReadKey();
+                Console.Clear();
+                Main(args);
+            }
+            else
+            {
+                Main(args);
+            }
+        }
+
+    }
+}
